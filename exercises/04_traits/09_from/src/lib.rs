7aa36368a5f74ae10c6650ccecab90ba3778587b
Sergei Belokon <srj.belokon@gmail.com>
2025-01-31 11:51:35 +1100

    chore: add IV/09 solution

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,10 +1,19 @@
 // TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
 
+#[allow(dead_code)]
 pub struct WrappingU32 {
     value: u32,
 }
 
+#[allow(unused_variables)]
+#[allow(dead_code)]
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
+}
+
+impl std::convert::From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        Self { value }
+    }
 }
