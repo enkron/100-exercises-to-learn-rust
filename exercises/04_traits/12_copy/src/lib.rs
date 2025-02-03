46d5c3b3fd53c4b9140edc5566ffd7f32e9e09a3
Sergei Belokon <srj.belokon@gmail.com>
2025-02-03 14:46:11 +1100

    chore: add IV/12 solution

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Debug, Clone, Copy)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,22 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl std::ops::Add for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value.wrapping_add(rhs.value),
+        }
+    }
+}
+
+impl PartialEq for WrappingU32 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
     }
 }
 
