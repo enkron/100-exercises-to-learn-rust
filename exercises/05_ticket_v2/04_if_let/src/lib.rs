8962ff8144bc93705b34e0fdbf2c5b533ee47bed
Sergei Belokon <srj.belokon@gmail.com>
2025-02-04 15:39:24 +1100

    chore: add V/04 solution

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 enum Shape {
     Circle { radius: f64 },
     Square { border: f64 },
@@ -7,8 +8,13 @@ enum Shape {
 impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
+    #[allow(dead_code)]
     pub fn radius(&self) -> f64 {
-        todo!()
+        if let Self::Circle { radius } = self {
+            *radius
+        } else {
+            panic!()
+        }
     }
 }
 
