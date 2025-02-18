4e2c5f70f29454148800f08a6370f5f8397ba181
Sergei Belokon <srj.belokon@gmail.com>
2025-02-18 14:44:50 +1100

    chore: add VII/10 solution

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -35,7 +35,18 @@ impl TicketStoreClient {
         Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {}
+    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {
+        let (tx, _) = sync_channel(1);
+
+        self.sender
+            .try_send(Command::Update {
+                patch: ticket_patch,
+                response_channel: tx,
+            })
+            .map_err(|_| OverloadedError)?;
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -63,7 +74,7 @@ enum Command {
     },
 }
 
-pub fn server(receiver: Receiver<Command>) {
+fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
@@ -85,7 +96,21 @@ pub fn server(receiver: Receiver<Command>) {
                 patch,
                 response_channel,
             }) => {
-                todo!()
+                let ticket = store.get_mut(patch.id).unwrap();
+
+                if let Some(title) = patch.title {
+                    ticket.title = title
+                }
+
+                if let Some(desc) = patch.description {
+                    ticket.description = desc
+                }
+
+                if let Some(status) = patch.status {
+                    ticket.status = status
+                }
+
+                let _ = response_channel.send(());
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
